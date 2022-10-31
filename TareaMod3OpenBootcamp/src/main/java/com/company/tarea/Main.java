975394e54cb4d243b3c6fdaeb29d4093f837a205
KevinEsborraz <109870246+KevinEsborraz@users.noreply.github.com>
2022-10-31 17:51:17 -0300

    Add files via upload

---
+++ TareaMod3OpenBootcamp/src/main/java/com/company/tarea/Main.java
@@ -0,0 +1,17 @@
+package com.company.tarea;
+
+public class Main {
+
+    public static void main (String[] args) {
+        int resultado;
+        resultado = suma(18, 34);
+
+        System.out.println(resultado);
+    }
+
+    private static int suma(int a, int b) {
+        return a + b;
+    }
+
+
+}
