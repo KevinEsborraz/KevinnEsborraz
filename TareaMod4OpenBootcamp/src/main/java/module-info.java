11fbc93dcdedc3b3bcca8afb1492bc85a5793dba
KevinEsborraz <109870246+KevinEsborraz@users.noreply.github.com>
2022-11-02 16:16:03 -0300

    Delete TareaMod4OpenBootcamp directory

--- TareaMod4OpenBootcamp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tareamod4openbootcamp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tareamod4openbootcamp to javafx.fxml;
-    exports com.example.tareamod4openbootcamp;
-}
