b5c883d087874ee8c739ac6847020522fda67013
KevinEsborraz <109870246+KevinEsborraz@users.noreply.github.com>
2022-11-04 01:17:01 -0300

    Delete TareaMod8OpenBootcamp directory

--- TareaMod8OpenBootcamp/src/main/java/com/example/tareamod8openbootcamp/Main.java
+++
@@ -1,2 +0,0 @@
-package com.example.tareamod8openbootcamp;
-
--- TareaMod8OpenBootcamp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tareamod8openbootcamp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tareamod8openbootcamp to javafx.fxml;
-    exports com.example.tareamod8openbootcamp;
-}
