8a8bd35a1df54a0ae4b62e7529a9c518a790bbb8
KevinEsborraz <109870246+KevinEsborraz@users.noreply.github.com>
2022-11-07 14:51:33 -0300

    Add files via upload

--- TareaMod9OpenBootcamp/src/main/java/com/example/tareamod9openbootcamp/Main.java
+++ TareaMod9OpenBootcamp/src/main/java/com/example/tareamod9openbootcamp/Main.java
@@ -3,24 +3,35 @@ package com.example.tareamod9openbootcamp;
 public class Main {
 
     public static void main (String[] args) {
-        Persona persona = new Persona();
-        persona.edad = 23;
-        persona.nombre = "Kevin";
-        persona.numeroTelefonico = 2352346;
+        Cliente cliente = new Cliente();
+        Trabajador trabajador = new Trabajador();
+        cliente.edad = 23;
+        cliente.nombre = "Kevin";
+        cliente.telefono = 2352346;
+        cliente.credito = 200.00;
+        System.out.println("Soy " + cliente.nombre + ", tengo " + cliente.edad + " años y mi telefono es " + cliente.telefono
+                + " y mi credito disponible es " + cliente.credito + "€");
+
+        trabajador.nombre = "Nahuel";
+        trabajador.edad = 23;
+        trabajador.telefono = 2945829;
+        trabajador.salario = 1000.00;
+        System.out.println("Soy " + trabajador.nombre + ", tengo " + trabajador.edad + " años y mi telefono es " + trabajador.telefono
+                + " y mi salario es de " + trabajador.salario + "€");
     }
 }
 
 class Persona {
     int edad;
     String nombre;
-    int numeroTelefonico;
+    int telefono;
 }
 
 class Cliente extends Persona {
-    int credito;
+    double credito;
 }
 
 class Trabajador extends Persona {
-    int salario;
+     double salario;
 
 }
