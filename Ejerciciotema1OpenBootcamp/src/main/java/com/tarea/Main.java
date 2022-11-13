103c9cf942bd68a046a2311596d85e6bd4ba4b13
KevinEsborraz <109870246+KevinEsborraz@users.noreply.github.com>
2022-11-13 02:29:12 -0300

    Add files via upload

---
+++ Ejerciciotema1OpenBootcamp/src/main/java/com/tarea/Main.java
@@ -0,0 +1,41 @@
+package com.tarea;
+
+import javax.swing.text.StringContent;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Numeros*/
+        byte var1 = 1;
+        System.out.println(var1);
+        short var2 = 2;
+        System.out.println(var2);
+        int var3 = 12;
+        System.out.println(var3);
+        long var4 = 23545;
+        System.out.println(var4);
+        float var5 = 6.7f;
+        System.out.println(var5);
+        double var6 = 33.45d;
+        System.out.println(var6);
+
+        /*Booleans*/
+        boolean var7 = false;
+        System.out.println(var7);
+        boolean var8 = true;
+        System.out.println(var8);
+
+        /*Text*/
+        char var9 = 'k';
+        System.out.println(var9);
+        String var10 = "kevin";
+        System.out.println(var10);
+
+
+
+
+
+
+
+    }
+}
