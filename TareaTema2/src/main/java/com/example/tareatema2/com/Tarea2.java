258e0e05a74a6c0235258ffca7a1895de7d9045e
KevinEsborraz <109870246+KevinEsborraz@users.noreply.github.com>
2022-11-13 13:53:27 -0300

    Add files via upload

---
+++ TareaTema2/src/main/java/com/example/tareatema2/com/Tarea2.java
@@ -0,0 +1,14 @@
+package com.example.tareatema2.com;
+
+
+public class Tarea2 {
+
+    public static void main(String[] args) {
+
+        int numero = 234;
+        double iva = numero * 0.21;
+        double suma = numero + iva;
+
+        System.out.println("El resultado con IVA es: " + suma);
+    }
+}
