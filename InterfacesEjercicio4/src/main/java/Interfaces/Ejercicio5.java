fc52f1e9d4e11c93fc45503011b75155f04d192e
KevinEsborraz <109870246+KevinEsborraz@users.noreply.github.com>
2022-11-15 17:54:51 -0300

    Add files via upload

---
+++ InterfacesEjercicio4/src/main/java/Interfaces/Ejercicio5.java
@@ -0,0 +1,46 @@
+package Interfaces;
+
+public class Ejercicio5 {
+
+    public interface CocheCRUD {
+
+        void save();
+        void findAll();
+        void delete();
+    }
+
+    public static class CocheCRUDImpl implements CocheCRUD{
+        @Override
+        public void save() {
+        }
+
+        @Override
+        public void findAll() {
+        }
+
+        @Override
+        public void delete() {
+        }
+
+        String save = "Guardando";
+        String findAll = "Buscar todo";
+        String delete = "Borrando";
+
+        @Override
+        public String toString() {
+            return "CocheCRUDImpl" +
+                    " save=" + save +
+                    ", findAll=" + findAll +
+                    ", delete=" + delete;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        CocheCRUD cocheCrud = new CocheCRUDImpl();
+        System.out.println(cocheCrud);
+
+    }
+
+}
+
