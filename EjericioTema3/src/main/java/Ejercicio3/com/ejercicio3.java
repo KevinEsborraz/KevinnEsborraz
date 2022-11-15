5089e836a1fa354f742a745b727925776e2f20d5
KevinEsborraz <109870246+KevinEsborraz@users.noreply.github.com>
2022-11-15 10:54:07 -0300

    Add files via upload

---
+++ EjericioTema3/src/main/java/Ejercicio3/com/ejercicio3.java
@@ -0,0 +1,11 @@
+package Ejercicio3.com;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Kevin", "Carla", "Nahuel", "Luciana"};
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+    }
+}
